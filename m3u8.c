ec6715fef93dfa3a35b74eb650d96b954f0eeff3
storage1 <storage1@feiying>
2012-06-15 17:41:49 +0800

    support any AVInputFormat media file

--- m3u8.c
+++ m3u8.c
@@ -22,36 +22,41 @@ target_duration* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  
 #include "libavformat/avformat.h"
 
 
-double get_ts_duration(const char *ts_file_name)
+double get_ts_duration(const char *segment_file_name)
 {
+    /*
     AVInputFormat *ifmt = av_find_input_format("mpegts");
     if (!ifmt) {
         fprintf(stderr, "Could not find MPEG-TS demuxer\n");
         return -1;
     }
+    */
 
+    AVInputFormat *ifmt = NULL;
     AVFormatContext *ic = NULL;
-    int ret = avformat_open_input(&ic, ts_file_name, ifmt, NULL);
+    int ret = avformat_open_input(&ic, segment_file_name, ifmt, NULL);
     if (ret != 0) {
-        fprintf(stderr, "Could not open input file %s, make sure it is an mpegts file: %d\n", ts_file_name, ret);
+        fprintf(stderr, "Could not open input file %s, make sure it is an mpegts file: %d\n", segment_file_name, ret);
         return -2;
     }
 
     if (avformat_find_stream_info(ic, NULL) < 0) {
         fprintf(stderr, "Could not read stream information\n");
-	return -3;
+    	return -3;
     }
 
     double video_stream_duration = 0.0;
     double audio_stream_duration = 0.0;
+    int video_flag = 0;
     int i;
     for (i = 0; i < ic->nb_streams; i++) {
         switch (ic->streams[i]->codec->codec_type) {
             case AVMEDIA_TYPE_VIDEO:
+                video_flag = 1;
                 video_stream_duration = (double)ic->streams[i]->duration * ic->streams[i]->time_base.num / ic->streams[i]->time_base.den;
                 break;
             case AVMEDIA_TYPE_AUDIO:
-                audio_stream_duration = ic->streams[i]->duration;
+                audio_stream_duration = (double)ic->streams[i]->duration * ic->streams[i]->time_base.num / ic->streams[i]->time_base.den;
                 break;
             default:
                 break;
@@ -60,43 +65,47 @@ double get_ts_duration(const char *ts_file_name)
 
     av_free(ic);
 
-    return video_stream_duration;
+    if (video_flag) {
+        return video_stream_duration;
+    } else {
+        return audio_stream_duration;
+    }
 }
 
 int main(int argc, char **argv)
 {
     const char *dir = NULL;
-    const char *prefix = NULL;
+    const char *pattern = NULL;
+    const char *file_type = NULL;
     unsigned int index = 0;
     const char *index_file_name = NULL;    
     unsigned int target_duration = 0;
     const char *http_prefix = NULL;
-    char ts_file_full_name[256];
-    char ts_file_name[128];
+    char segment_file_path[256];
+    char segment_file_name[128];
 
     FILE *index_fp;
     char write_buf[512];
 
     if (argc < 7 ) {
-        fprintf(stderr, "Usage: %s <MPEG-TS dir> <MPEG-TS file prefix> <start index> <output m3u8 index file> <target duration> <http prefix>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <media dir> <media file pattern> <start index> <output m3u8 index file> <target duration> <http pattern>\n", argv[0]);
         exit(1);
     }
     
     dir = argv[1];
-    prefix = argv[2];
+    pattern = argv[2];
     index = atoi(argv[3]);
     index_file_name = argv[4];
     target_duration = atoi(argv[5]);
     http_prefix = argv[6];
 
     fprintf(stdout, "MPEG-TS dir: %s\n", dir);
-    fprintf(stdout, "MPEG-TS prefix: %s\n", prefix);
+    fprintf(stdout, "MPEG-TS pattern: %s\n", pattern);
     fprintf(stdout, "start index: %d\n", index);
     fprintf(stdout, "index file name: %s\n", index_file_name);
     fprintf(stdout, "target duration: %d\n", target_duration);
-    fprintf(stdout, "http prefix: %s\n", http_prefix );
+    fprintf(stdout, "http pattern: %s\n", http_prefix );
   
-
     index_fp = fopen(index_file_name, "w");
     if (!index_fp) {
         fprintf(stderr, "Could not open temporary m3u8 index file (%s), no index file will be created\n", index_file_name);
@@ -113,15 +122,15 @@ int main(int argc, char **argv)
     av_register_all();
 
     while(1){
-        snprintf(ts_file_name, sizeof(ts_file_name), "%s%u.ts", prefix, index);
-        snprintf(ts_file_full_name, sizeof(ts_file_full_name), "%s%s", dir, ts_file_name);
+        snprintf(segment_file_name, sizeof(segment_file_name), pattern, index);
+        snprintf(segment_file_path, sizeof(segment_file_path), "%s%s", dir, segment_file_name);
         
-        double duration = get_ts_duration(ts_file_full_name); 
+        double duration = get_ts_duration(segment_file_path); 
         if (duration < 0){
             break;
         }
 
-        snprintf(write_buf, sizeof(write_buf), "#EXTINF:%u,\n%s%s\n", (unsigned int)round(duration), http_prefix, ts_file_name);
+        snprintf(write_buf, sizeof(write_buf), "#EXTINF:%u,\n%s%s\n", (unsigned int)round(duration), http_prefix, segment_file_name);
         if (fwrite(write_buf, strlen(write_buf), 1, index_fp) != 1) {
 	    fprintf(stderr, "Could not write to m3u8 index file, will not continue writing to index file\n");
 	    break;
